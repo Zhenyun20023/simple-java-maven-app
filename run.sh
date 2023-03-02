63002c417824dc4f4272dfa377a18a000212930a
zhenyun <test@gmail.com>
2023-03-02 14:15:18 -0800

    updated run.sh.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd src/main/java;
 javac com/mycompany/app/*.java;
-java com.mycompany.app.App -cp . -Xlog:gc+age*,safepoint:file=/tmp/logs/gc.log:time,level,tags:filecount=5,filesize=100M -Xms500M -Xmx500M -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:NativeMemoryTracking=summary -XX:+AlwaysPreTouch -XX:MaxDirectMemorySize=1100M
+java com.mycompany.app.App -cp . -Xlog:gc+age*,safepoint:file=/tmp/logs/gc.log:time,level,tags:filecount=5,filesize=100M -Xms500M -Xmx500M -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:NativeMemoryTracking=summary -XX:+AlwaysPreTouch -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/logs/ -XX:MaxDirectMemorySize=1100M
 
