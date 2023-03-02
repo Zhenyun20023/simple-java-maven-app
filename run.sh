1b43fa91fde8ff7bf0a0f47b175e22a1e086502a
zhenyun <test@gmail.com>
2023-03-02 13:33:43 -0800

    updated run.sh.

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 cd src/main/java;
-rm com/mycompany/app/*.class
-echo " deleted old classes.";
 javac com/mycompany/app/*.java;
 java com.mycompany.app.App -cp . -Xlog:gc+age*,safepoint:file=/tmp/logs/gc.log:time,level,tags:filecount=5,filesize=100M -Xms500M -Xmx500M -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:NativeMemoryTracking=summary -XX:+AlwaysPreTouch
