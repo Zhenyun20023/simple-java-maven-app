f777c7bcb97cf89b93aa82b8da3e200b55e1434b
zhenyun <test@gmail.com>
2023-03-02 18:42:31 -0800

    add

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -21,7 +21,7 @@ public class App {
             file = new RandomAccessFile("/tmp/file.400MB", "rw");
             FileChannel channel = file.getChannel();
             MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, channel.size());
-            System.out.printf(" The file size: %d \n", filesize);
+            System.out.printf(" The file size: %d \n", channel.size());
             int offset = mmapMB * 1024 * 1024; //
             channel.position(offset);
             buffer.put("some data".getBytes());
