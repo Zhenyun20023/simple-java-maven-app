dbb5bfbe9ff6b1b969b3bcac35f491d5dfc4826d
zhenyun <test@gmail.com>
2023-03-06 12:35:22 -0800

    updated

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 cd src/main/java;
 javac com/mycompany/app/*.java;
-java -cp . -Xlog:gc+age*,safepoint:file=/tmp/logs/gc.log:time,level,tags:filecount=5,filesize=100M -Xms500M -Xmx500M -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:NativeMemoryTracking=summary -XX:+AlwaysPreTouch -Dcom.sun.management.jmxremote.port=8080 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/logs/ -XX:MaxDirectMemorySize=400M com.mycompany.app.App
+java -cp . -Xlog:gc+age*,safepoint:file=/tmp/logs/gc.log:time,level,tags:filecount=5,filesize=100M -Xms500M -Xmx500M -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:NativeMemoryTracking=summary -XX:+AlwaysPreTouch -Dcom.sun.management.jmxremote.port=8123 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/logs/ -XX:MaxDirectMemorySize=400M com.mycompany.app.App
 
