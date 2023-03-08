fe3ec9a373b3b024767eefda5f757218f424b674
zhenyun <test@gmail.com>
2023-03-08 13:58:06 -0800

    updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,8 +5,8 @@ import java.nio.channels.*;
 
 
 public class App {
-    public static int numThreads = 50;
-    public static int directMB = 5; // 10MB, each thread;
+    public static int numThreads = 100;
+    public static int directMB = 10; // 10MB, each thread;
     public static int mmapMB = 400; // mmap file;
     public static Thread[] threads = new Thread[numThreads];
 
