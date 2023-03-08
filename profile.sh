2d42ab19587a6f6b5622c753df254e29cc895397
zhenyun <test@gmail.com>
2023-03-07 21:16:09 -0800

    updated

--- profile.sh
+++ profile.sh
@@ -1,5 +1,9 @@
 
 pid=$(jps | grep App | awk '{print $1}')
+mkdir ${pid}
+cd ${pid}
 jcmd $pid VM.native_memory > native_memory.txt
 pmap -X $pid > pmap.txt
 cat /proc/${pid}/maps > maps.txt
+perl /home/ec2-user/testing/javaJniMemUsage/javaJniMemUsage.pl maps.txt  > maps.perl.txt
+
