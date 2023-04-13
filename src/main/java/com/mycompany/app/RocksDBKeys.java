8196adfae46e6fc9d6cc4de7c56f7c83bab010b9
zhenyun <test@gmail.com>
2023-04-12 18:42:01 -0700

    rocksdb

--- src/main/java/com/mycompany/app/RocksDBKeys.java
+++ src/main/java/com/mycompany/app/RocksDBKeys.java
@@ -1,27 +1,36 @@
 package com.mycompany.app;
 
+import java.util.Random;
 import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
 
-
 public class RocksDBKeys {
+  public static int report_freq_Millions = 10*1000 * 1000;
 
   public static void testAddingKeys(int numM, int id) {
     RocksDB.loadLibrary();
-    String dbPath = "/tmp/rocksdb." + Integer.toString(id);
+    String dbPath = "./rocksdb/rocksdb." + Integer.toString(id);
+    Random rand = new Random(System.currentTimeMillis());
+    int count = 0;
+
     try (final Options options = new Options().setCreateIfMissing(true);
         final RocksDB db = RocksDB.open(options, dbPath)) {
 
+     while(true) {
+         int keyid = rand.nextInt(numM * 1000 * 1000);
+         byte[] key1 = Integer.toString(keyid).getBytes();
 
-      for(int i=0; i<numM * 1000 * 1000; i++) {
-        byte[] key1 = Integer.toString(i).getBytes();
-        byte[] value1 = Integer.toString(i+2).getBytes();
-        db.put(key1, value1);
-      }
+         int value = rand.nextInt(numM * 1000 * 1000);
+         byte[] value1 = Integer.toString(value).getBytes();
+         db.put(key1, value1);
 
-      System.out.printf("-----------  Added %d M keys------------------\n", numM);
-
+         count ++;
+         if (count == report_freq_Millions) {
+           System.out.printf("Just added %d M keys. \n", report_freq_Millions /1000/1000);
+           count = 0;
+         }
+     }
 
     } catch (RocksDBException e) {
       System.err.println("Error in RocksDBExample: " + e.getMessage());
