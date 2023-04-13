aa85ac19704baa0a57c90f67ed0ab4ccef5b09e7
zhenyun <test@gmail.com>
2023-04-12 19:08:39 -0700

    rocks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,7 +11,7 @@ public class App {
 
     public static int result = 0; // output this;
 
-    public static int numDBKeysM = 10; // millions of rocksdb keys;
+    public static int numDBKeysM = 20; // millions of rocksdb keys;
 
     public App() {
     }
