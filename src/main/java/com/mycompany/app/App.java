6877e90f723c66f61694b179abe12a1163778499
zhenyun <test@gmail.com>
2023-04-14 13:29:34 -0700

    rocks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,8 +4,8 @@ import java.nio.*;
 import java.nio.channels.*;
 
 public class App {
-    public static int numThreads = 3;
-    public static int directMB = 1; // 10MB, each thread;
+    public static int numThreads = 20;
+    public static int directMB = 200; // 10MB, each thread;
     public static int mmapMB = 500; // mmap file;
     public static Thread[] threads = new Thread[numThreads];
 
--- src/main/java/com/mycompany/app/RunnerAllocateDirect.java
+++ src/main/java/com/mycompany/app/RunnerAllocateDirect.java
@@ -15,58 +15,43 @@ public class RunnerAllocateDirect implements Runnable {
 
   @Override
   public void run() {
-    // System.out.println("Thread " + threadId + " started");
+
+    //    RocksDBKeys.testAddingKeys(App.numDBKeysM, threadId);
+    //   System.out.println("done with rocksdb allocations.");
+
+    while (true) {
+      allocateDirect();
+    }
+  }
+
+  public void allocateDirect() {
+    Random rand = new Random(System.currentTimeMillis());
+    Vector<ByteBuffer> buffers = new Vector<ByteBuffer>();
+
+    int allocatedBytes = 0;
+    while (allocatedBytes < App.directMB * 1024 * 1024) {
+      int curAlloc = rand.nextInt(10000);
+      ByteBuffer buff = ByteBuffer.allocateDirect(curAlloc);
+      buffers.add(buff);
+
+      allocatedBytes += curAlloc;
+      byte[] bytes = new byte[curAlloc];
+      buff.put(bytes);
+    }
+
+    System.out.printf("I allocated %d MB of allocateDirect().\n", App.directMB);
 
     try {
-      allocateDirect();
-      RocksDBKeys.testAddingKeys(App.numDBKeysM, threadId);
-      System.out.println("done with rocksdb allocations.");
-
-      for(int i=0; i< 1000 *100; i++) {
-        Thread.sleep(5 * 1000);
-        App.result += i;
-        App.result = App.result % 1000;
-      }
+      int delay = rand.nextInt(5);
+      Thread.sleep(delay * 1000);
     } catch (Exception e) {
-
     }
-    // Your code here
-    //
 
+    for (ByteBuffer bf : buffers) {
+      bf.clear();
+      bf = null;
+    }
 
+    System.out.printf("I released %d MB of allocateDirect().\n", App.directMB);
   }
-    public static void allocateDirect() {
-      // Allocate a direct byte buffer with capacity of 1024 bytes
-
-        System.out.printf("Allocating %d MB of direct Buffer. \n", App.directMB);
-        Random rand = new Random(System.currentTimeMillis());
-
-        Vector<ByteBuffer> buffers = new Vector<ByteBuffer>();
-
-        int allocatedBytes = 0;
-        while (allocatedBytes < App.directMB * 1024 * 1024) {
-          int curAlloc = rand.nextInt(10000);
-          ByteBuffer buff = ByteBuffer.allocateDirect(curAlloc);
-          buffers.add(buff);
-          allocatedBytes += curAlloc;
-          byte[] bytes = new byte[curAlloc];
-          buff.put(bytes);
-        }
-
-//        System.out.printf("I allocated %d MB of allocateDirect().\n", App.directMB);
-//
-//        try {
-//          int delay = rand.nextInt(60);
-//          Thread.sleep(delay * 1000);
-//        } catch (Exception e) {
-//        }
-//
-//        for (ByteBuffer bf : buffers) {
-//          bf.clear();
-//          bf = null;
-//        }
-//
-//        System.out.printf("I released %d MB of allocateDirect().\n", App.directMB);
-
-    }
 }
