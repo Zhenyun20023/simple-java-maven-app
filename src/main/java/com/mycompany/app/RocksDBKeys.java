d3b1345ab63c5c471af3634eb3551e144b326c86
zhenyun <test@gmail.com>
2023-04-13 18:45:41 -0700

    rocks

--- src/main/java/com/mycompany/app/RocksDBKeys.java
+++ src/main/java/com/mycompany/app/RocksDBKeys.java
@@ -5,18 +5,34 @@ import java.util.Random;
 import org.rocksdb.Options;
 import org.rocksdb.RocksDB;
 import org.rocksdb.RocksDBException;
-
+import org.rocksdb.BlockBasedTableConfig;
+import org.rocksdb.Cache;
 public class RocksDBKeys {
   public static int report_freq_Millions = 1*1000 * 1000;
 
   public static void testAddingKeys(int numM, int id) {
     RocksDB.loadLibrary();
+    BlockBasedTableConfig tableOptions = new BlockBasedTableConfig();
+    tableOptions.setBlockCacheSize(1073741824L); // 1 GB block cache size
+
+    Options options = new Options();
+    options.setMaxOpenFiles(-1); // -1 means use the OS default
+    options.setCreateIfMissing(true);
+    options.setAllowMmapWrites(true);
+    options.setAllowMmapReads(true);
+    options.setTableFormatConfig(tableOptions);
+    options.setWriteBufferSize(267108864L); // 264 MB write buffer size
+    options.setMaxWriteBufferNumber(3); // 3 write buffers
+    options.setMinWriteBufferNumberToMerge(2);
+    options.setTargetFileSizeBase(268435456L); // 256 MB
+    options.setTargetFileSizeMultiplier(1);
+    options.setNumLevels(2);
+
     String dbPath = "./rocksdb/rocksdb." + Integer.toString(id);
     Random rand = new Random(System.currentTimeMillis());
     int count = 0;
 
-    try (final Options options = new Options().setCreateIfMissing(true);
-        final RocksDB db = RocksDB.open(options, dbPath)) {
+    try (final RocksDB db = RocksDB.open(options, dbPath)) {
 
      while(true) {
          int keyid = rand.nextInt(numM * 1000 * 1000);
